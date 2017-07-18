60cfc601f9658cd1b80dc561eb0d21dc527c6f71
syamimi azizudin <A149241@siswa.ukm.edu.my>
2017-07-18 16:50:55 +0800

    Initial commit

---
+++ wp-config.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'tigress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '& &a~(6K ,;|4!+Uetu*Lj%69y}m-ZQpbzj-*IR|kFd6(.dpcOdTVz|j~lWjZ>jG');
+define('SECURE_AUTH_KEY',  'A>7ht,T/nxbQGuJ|R%kKIt8Xb(l_!gro%-aw(.=+|{?q#lP*|YCcZX=92/{| j%p');
+define('LOGGED_IN_KEY',    'sqmaFtPXF1i(7:w&Ao<5QI(rzWZ`}Ia<|a-K15*R:]InN:4+F,K0{Sm61u $475O');
+define('NONCE_KEY',        'jm>sbI+je&]B`BKW>zme|^{AHOl.1[aW!$MN/~ (`;<Sh-Nd~OMP9eQ q_`.@,b(');
+define('AUTH_SALT',        'pUrL$GBij2oCOv3GIuux8GzXtP)AdB9M-Z66HmdcR&C<It<.TGLRG|9,X.-RVTY)');
+define('SECURE_AUTH_SALT', 'FQ*_=a.yXpR6KG}oa~io|tJC!:u@s#[+ukZcmZu)0%-Hm$61/QN:W2J^V5bJ.2Nl');
+define('LOGGED_IN_SALT',   '>bu4%kIeTEP,+KKUK|{V>KiI8*@z#:u^z2V9z/:t1+dkJAc.F l#kB5s@&zf,NfH');
+define('NONCE_SALT',       'tCxfa^6pc5p9M]ib[_aq9>tn-SSugPh[/X?e, [Z zbdzN@+`gt8g}])eyY{C5(|');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+define('FS_METHOD', 'direct');
---
+++ wp-content/themes/siteorigin-north/inc/contributors.php
@@ -0,0 +1,38 @@
+<?php
+return json_decode( '{
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 5880,
+		"score": 305.939996336349,
+		"percent": 83.39954152173812
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 479,
+		"score": 48.34404078799464,
+		"percent": 13.178632690425808
+	},
+	"0f7f79a15dafbc4c1b64dfdc79c6937d": {
+		"name": "adiraomj",
+		"email": "0f7f79a15dafbc4c1b64dfdc79c6937d",
+		"loc": 380,
+		"score": 9.375155085539859,
+		"percent": 2.555676424110356
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 45,
+		"score": 3.1488222556614214,
+		"percent": 0.8583720193514521
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 2,
+		"score": 0.0285301413647851,
+		"percent": 0.007777344374279037
+	}
+}', true );
